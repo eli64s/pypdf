324d6e1ada3a2dcca9cfb579808a5f8595597db3
eli64s <43382407+eli64s@users.noreply.github.com>
2020-12-16 09:57:09 -0600

    Add files via upload

--- PDF_Parser.py
+++ PDF_Parser.py
@@ -4,60 +4,56 @@ import re
 
 class ParsePDF: 
     # Constructor 
-    def __init__(self, path, regex_date): 
+    def __init__(self, path, regex_date_pattern): 
         self.path = path 
-        self.regex_date = regex_date
-    # static methods work independent of class object 
+        self.regex_date_pattern = regex_date_pattern
+
     @staticmethod
     def find_pdf_text_to_delete(lines): 
-        """ Get all the lines of the PDF document """
+        """
+        Static method working independent of the class object.
+        Get all the lines of the PDF document.
+        """
         for line in lines: 
-            # matching the regex to each line 
-            if re.search(regex_date, line, re.IGNORECASE): 
-                search = re.search(regex_date, line, re.IGNORECASE) 
-                # yields creates a generator to return values in between function iterations 
+            # Look for regex match in each line
+            if re.search(regex_date_pattern, line, re.IGNORECASE): 
+                search = re.search(regex_date_pattern, line, re.IGNORECASE) 
+                # Yield creates a generator to return values in between function iterations 
                 yield search.group(1) 
   
     def edit_pdf_file(self): 
-        """
-        """
-        # Open the PDF file to edit 
+        """ Sets today's date in the PDF document. """
+        # Open the PDF file for editing 
         document = fitz.open(self.path) 
           
         # Iterate through the pages of the PDF document 
         for page in document: 
             # _wrapContents is needed for fixing alignment issues with rect boxes in some cases where there is alignment issue 
             page._wrapContents() 
-            # geting the rect boxes which consists the matching email regex 
-            sensitive = self.find_pdf_text_to_delete(page.getText("develop").split('\n')) 
+            # geting the rect boxes which consists the matching date regex pattern
+            text_date = self.find_pdf_text_to_delete(page.getText("develop").split('\n')) 
 
-            for data in sensitive: 
+            for data in text_date: 
                 areas = page.searchFor(data) 
-                # drawing outline over sensitive datas 
                 [page.addRedactAnnot(area) for area in areas] 
-            # applying the edit_pdf_file 
             page.apply_redactions() 
               
-        # Insert the new date into the PDF file
+        # Define today's date and the coordinates to set the date in the PDF file
         page = document[0]         
         pdf_coordinates = fitz.Point(453, 58) 
         curr_date = date.today()
         todays_date = f"""{curr_date.strftime("%B")} {curr_date.day}, {curr_date.year}"""
 
-        rc = page.insertText(
-            pdf_coordinates,  
-            todays_date,  
-            fontname = "helv",  
-            fontsize = 9.5,  
-            rotate = 0,  
-        )
+        # Set the new text in the PDF file
+        set_text = page.insertText(pdf_coordinates, todays_date, fontname = "helv", fontsize = 9.5)
         
-        document.save(path.format("test2"))    
+        # Save the updated PDF file 
+        document.save(path.format("PDF_After"))    
 
 
-# Main Function that runs the script
+# Main method to runs the script
 if __name__ == "__main__": 
     path = r"C:\Users\esalam1\Desktop\Python\Scripts\{}.pdf"
-    regex_date = r"(Nov(?:ember)?\s([1-9]|([12][0-9])|(3[01])),\s\d\d\d\d)"
-    parse_pdf = ParsePDF(path.format("test"), regex_date) 
+    regex_date_pattern = r"(Nov(?:ember)?\s([1-9]|([12][0-9])|(3[01])),\s\d\d\d\d)"
+    parse_pdf = ParsePDF(path.format("PDF_Before"), regex_date_pattern) 
     parse_pdf.edit_pdf_file()
